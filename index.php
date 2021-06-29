cd003833642ae526b0e4e1cba974d0c7536ece33
mmmarceloo <83249188+mmmarceloo@users.noreply.github.com>
2021-06-28 22:26:38 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+ 
+ $Array=[];
+ $ArrayRepetido=[];
+ echo "Array sorteado = [";
+ for ($i = 0; $i<20; $i++){
+     
+    $Array[$i]=rand(1,10);
+    echo $Array[$i].",";
+ }  
+ echo "]<br/>";
+
+
+
+
+
+
